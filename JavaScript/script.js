75b30519066456b5210c77018645671d14c79513
Sigurd Drivstuen <sigdriv06@gmail.com>
2023-10-19 18:53:58 +0200

    add check and uncheck
    add check and uncheck markers also a delete button also fixed the count
    
    Count undone task and done task #11
    Check and uncheck mark and delete button #14

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,30 +1,60 @@
 let list = document.getElementById("list");
 let textNumber = document.getElementById("taskNumber");
+let li = document.createElement("li");
 
 let countTasks = 0;
 let doneTask = 0;
 
 function addTask (task) {
-    let li = document.createElement("li");
     li.appendChild(document.createTextNode(task.input.value));
     list.appendChild(li);
+    li.id = "unchecked";
 
-    
+    let span = document.createElement("span");
+    span.innerHTML = "\u00D7";
+    li.appendChild(span);
+
+
     countTasks++;
-    
     saveData();
-    document.getElementById("taskNumber").innerHTML = `${localStorage.getItem("countTasks")} / ${localStorage.getItem("doneTask")} fullført`;
+    task.input.value = "";
 }
+
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI" && e.target.id == "unchecked") {
+        li.classList = ('list')
+        e.target.classList.toggle("checked");
+        e.target.id = "checked";
+        doneTask++;
+        saveData();
+    } else if (e.target.tagName === "LI" && e.target.id == "checked") {
+        e.target.classList.toggle("unchecked");
+        e.target.id = "unchecked";
+        doneTask--;
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    } 
+}, false);
+
+
 
 function saveData() {
     localStorage.setItem("tasks", list.innerHTML);
     localStorage.setItem("countTasks", countTasks);
     localStorage.setItem("doneTask", doneTask);
+    document.getElementById("taskNumber").innerHTML = `${localStorage.getItem("countTasks")} / ${localStorage.getItem("doneTask")} fullført`;
 }
 
 function loadData() {
     list.innerHTML = localStorage.getItem("tasks");
-    textNumber.innerHTML = `${localStorage.getItem("countTasks")} / ${localStorage.getItem("doneTask")} fullført`;
+    if (localStorage.getItem("countTasks") == null) {
+        textNumber.innerHTML = `Du har ikke lagt til noen oppgaver enda`;
+    }
+    else {
+        textNumber.innerHTML = `${localStorage.getItem("countTasks")} / ${localStorage.getItem("doneTask")} fullført`;
+    }
 }
 
 loadData();
