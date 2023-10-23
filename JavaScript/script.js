66df3cd0b6cf4a9327d629688ad721130fbdacd0
Sigurd Drivstuen <sigdriv06@gmail.com>
2023-10-23 08:20:53 +0200

    Add a new method
    Add a new method to mark task as done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,22 @@
 let list = document.getElementById("list");
 let textNumber = document.getElementById("taskNumber");
-let li = document.createElement("li");
 
 let countTasks = 0;
 let doneTask = 0;
 
-function addTask (task) {
+function addTask(task) {
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    li.appendChild(checkbox);
     li.appendChild(document.createTextNode(task.input.value));
-    list.appendChild(li);
-    li.id = "unchecked";
+    li.className = "unchecked";
 
     let span = document.createElement("span");
     span.innerHTML = "\u00D7";
     li.appendChild(span);
-
+    list.appendChild(li);
 
     countTasks++;
     saveData();
@@ -21,30 +24,34 @@ function addTask (task) {
 }
 
 list.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI" && e.target.id == "unchecked") {
-        li.classList = ('list')
-        e.target.classList.toggle("checked");
-        e.target.id = "checked";
-        doneTask++;
-        saveData();
-    } else if (e.target.tagName === "LI" && e.target.id == "checked") {
-        e.target.classList.toggle("unchecked");
-        e.target.id = "unchecked";
-        doneTask--;
+    if (e.target.tagName === "LI") {
+        const checkbox = e.target.querySelector("input[type='checkbox']");
+        if (checkbox && checkbox.checked === false) {
+            e.target.className = "checked";
+            doneTask--;
+        } else if (checkbox && checkbox.checked === true) {
+            e.target.className = "unchecked";
+            doneTask++;
+        }
         saveData();
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        countTasks--;
+        doneTask--;
         saveData();
-    } 
+    }
 }, false);
-
-
 
 function saveData() {
     localStorage.setItem("tasks", list.innerHTML);
     localStorage.setItem("countTasks", countTasks);
     localStorage.setItem("doneTask", doneTask);
-    document.getElementById("taskNumber").innerHTML = `${localStorage.getItem("countTasks")} / ${localStorage.getItem("doneTask")} fullført`;
+    if (localStorage.getItem("countTasks") == null) {
+        textNumber.innerHTML = `Du har ikke lagt til noen oppgaver enda`;
+    }
+    else {
+        textNumber.innerHTML = `${doneTask} / ${countTasks} fullført`;
+    }
 }
 
 function loadData() {
@@ -53,7 +60,7 @@ function loadData() {
         textNumber.innerHTML = `Du har ikke lagt til noen oppgaver enda`;
     }
     else {
-        textNumber.innerHTML = `${localStorage.getItem("countTasks")} / ${localStorage.getItem("doneTask")} fullført`;
+        textNumber.innerHTML = `${doneTask} / ${countTasks} fullført`;
     }
 }
 
